75ea90cca988d612871fa897cf91a88c6740c7e0
Ahmad Azuddin <exectraquasar@gmail.com>
2022-10-16 22:59:09 +0800

    add sample json api code

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
-func main()  {
-	fmt.Println("Hello, World!")
+func ReturnJsonResponse(res http.ResponseWriter, httpCode int, resMessage []byte) {
+	res.Header().Set("Content-type", "application/json")
+	res.WriteHeader(httpCode)
+	res.Write(resMessage)
+}
+
+func TestHandler(res http.ResponseWriter, req *http.Request) {
+	HandlerMessage := []byte(`{
+  "success": true,
+  "message": "The server is running properly"
+}`)
+
+	ReturnJsonResponse(res, http.StatusOK, HandlerMessage)
+}
+
+func main() {
+	http.HandleFunc("/health", TestHandler)
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
